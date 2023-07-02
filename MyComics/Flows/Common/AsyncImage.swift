7dcf121d6c964ffcc5ac3cd2790595bdad294470
AntonSVTSV <freshbreathe@gmail.com>
2023-07-02 18:20:49 +0700

    Add AsyncImage

---
+++ MyComics/Flows/Common/AsyncImage.swift
@@ -0,0 +1,13 @@
+//
+//  AsyncImage.swift
+//  MyComics
+//
+//  Created by Антон Сивцов on 02.07.2023.
+//
+
+import Foundation
+
+struct AsyncImage: Hashable {
+    let imageURL: String?
+    let imageExtension: String?
+}
