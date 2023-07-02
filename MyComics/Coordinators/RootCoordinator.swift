abe8969f6d942cd479d13eb6cf3248b2a706200f
AntonSVTSV <freshbreathe@gmail.com>
2023-07-02 18:12:50 +0700

    Use localize method on localization keys

--- MyComics/Coordinators/RootCoordinator.swift
+++ MyComics/Coordinators/RootCoordinator.swift
@@ -14,15 +14,15 @@ final class RootCoordinator {
     func start(in window: UIWindow) {
         let charactersVC = UINavigationController(rootViewController: CharactersModuleBuilder.build())
         charactersVC.tabBarItem.image = UIImage(systemName: "bubble.left.and.bubble.right")
-        charactersVC.tabBarItem.title = LocalizationKeys.charactersTabItem.rawValue.localized
+        charactersVC.tabBarItem.title = LocalizationKeys.localized(.charactersTabItem)
         
         let comicsVC = UINavigationController(rootViewController: ComicsModuleBuilder.build())
         comicsVC.tabBarItem.image = UIImage(systemName: "person.circle")
-        comicsVC.tabBarItem.title = LocalizationKeys.comicsTabItem.rawValue.localized
+        comicsVC.tabBarItem.title = LocalizationKeys.localized(.comicsTabItem)
         
         let profileVC = UINavigationController(rootViewController: ProfileModuleBuilder.build())
         profileVC.tabBarItem.image = UIImage(systemName: "gear")
-        profileVC.tabBarItem.title = LocalizationKeys.profileTabItem.rawValue.localized
+        profileVC.tabBarItem.title = LocalizationKeys.localized(.profileTabItem)
         
         let tabBarVC = UITabBarController()
         tabBarVC.viewControllers = [charactersVC, comicsVC, profileVC]
