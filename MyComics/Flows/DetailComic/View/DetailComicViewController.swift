0efe39e9a9b2478ef22ae7d9391dc1a50337c5ab
Andrey91 <mistaandrey@icloud.com>
2023-07-19 15:01:54 +0500

    Add not final UI on detail comic view

--- MyComics/Flows/DetailComic/View/DetailComicViewController.swift
+++ MyComics/Flows/DetailComic/View/DetailComicViewController.swift
@@ -13,6 +13,29 @@ final class DetailComicViewController: UIViewController {
     
     private let presenter: DetailComicViewOutput
     
+    private let comicImageView: AsyncImageView = {
+        let comicImageView = AsyncImageView()
+        comicImageView.contentMode = .scaleAspectFit
+        return comicImageView
+    }()
+    
+    private let comicTitleLabel: UILabel = {
+        let comicTitleLabel = UILabel()
+        comicTitleLabel.textColor = .white
+        comicTitleLabel.font = .systemFont(ofSize: 23, weight: .bold)
+        comicTitleLabel.textAlignment = .center
+        return comicTitleLabel
+    }()
+    
+    private let comicDescriptionLabel: UILabel = {
+        let comicDescriptionLabel = UILabel()
+        comicDescriptionLabel.textColor = .white
+        comicDescriptionLabel.textAlignment = .center
+        comicDescriptionLabel.font = .systemFont(ofSize: 17, weight: .light)
+        comicDescriptionLabel.numberOfLines = 10
+        return comicDescriptionLabel
+    }()
+    
     // MARK: - Init
     
     init(presenter: DetailComicViewOutput) {
@@ -28,7 +51,50 @@ final class DetailComicViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureUI()
         presenter.viewIsReady()
+    }
+    
+    // MARK: - Private methods
+    
+    @objc func tapLikeNavBarButton() {
+        print("tapLike")
+    }
+    
+    private func configureUI() {
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(image: .likeBarButtonItem, style: .plain, target: self, action: #selector(tapLikeNavBarButton))
+        view.backgroundColor = .commonBackground
+        view.addSubview(comicImageView)
+        view.addSubview(comicTitleLabel)
+        view.addSubview(comicDescriptionLabel)
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        let safeArea = self.view.safeAreaLayoutGuide
+        
+        comicImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            comicImageView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            comicImageView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            comicImageView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            comicImageView.heightAnchor.constraint(equalTo: comicImageView.widthAnchor)
+        ])
+        
+        comicTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            comicTitleLabel.topAnchor.constraint(equalTo: comicImageView.bottomAnchor, constant: 20),
+            comicTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            comicTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+        
+        comicDescriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            comicDescriptionLabel.topAnchor.constraint(equalTo: comicTitleLabel.bottomAnchor, constant: 20),
+            comicDescriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            comicDescriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
     }
 }
 
